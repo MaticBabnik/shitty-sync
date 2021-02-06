87d9319e3a5ed59677e35894ee441253015e1656
Matic Babnik <matic.babnik@outlook.com>
2021-02-06 05:06:24 +0100

    🔌 websocket.io pog

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueSocketIO from '@maticbabnik/vue3-socket.io' //I am really sorry i had to do this autism
+import SocketIO from 'socket.io'
 
-createApp(App).use(router).mount('#app')
+
+createApp(App).use(router).use(new VueSocketIO({
+    debug:true,
+    connection: SocketIO()
+})).mount('#app')
