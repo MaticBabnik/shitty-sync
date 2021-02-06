49e1023392e0a0370776c3d58199e620905e22a6
Matic Babnik <matic.babnik@outlook.com>
2021-02-06 15:57:43 +0100

    🐛 fixed websockets

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import VueSocketIO from '@maticbabnik/vue3-socket.io' //I am really sorry i had to do this autism
-//import SocketIO from 'socket.io'
+import VueSocketIO from '@maticbabnik/vue-socket.io' //I am really sorry i had to do this autism
+import {io} from 'socket.io-client'
 
 
 window.vue = createApp(App).use(router).use(new VueSocketIO({
     debug:true,
-    connection: 'http://yeet.si/'
+    connection: io()
 })).mount('#app')
