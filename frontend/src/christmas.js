e8f87bf463242e65a94b5a49459eb1961a0de6b3
Matic Babnik <matic.babnik@outlook.com>
2022-02-25 04:37:33 +0100

    Retiring the `deployment` branch (#26)
    * Dev/particles fix (#23)
    
    * 🔄Updating readme
    
    * 🎅 Christmas theme
    
    * 🎅 Particles.js wasn't playing nice with 🦊 firefox
    
    Co-authored-by: Blaž <22869613+xBlaz3kx@users.noreply.github.com>
    Co-authored-by: Martin Hanzlowsky <martin.hanzlowskyg@gmail.com>
    
    * No more xmas (#25)
    
    * 🔄Updating readme
    
    * ☠🎅 no more xmas
    
    Co-authored-by: Blaž <22869613+xBlaz3kx@users.noreply.github.com>
    Co-authored-by: Martin Hanzlowsky <martin.hanzlowskyg@gmail.com>
    
    * 📦 Actions on `master`
    
    Co-authored-by: Blaž <22869613+xBlaz3kx@users.noreply.github.com>
    Co-authored-by: Martin Hanzlowsky <martin.hanzlowskyg@gmail.com>

--- frontend/src/christmas.js
+++ frontend/src/christmas.js
@@ -1,37 +1,36 @@
-if (!window.particlesJS) {
-    console.error("particlesJS broke")
-} else
-    window.particlesJS("snow", {
-        particles: {
-            number: { value: 400, density: { enable: true, value_area: 800 } },
-            color: { value: "#fff" },
-            shape: {
-                type: ["circle","polygon"],
-                stroke: { width: 0, color: "#000000" },
-                polygon: { nb_sides: 5, },
-            },
-            opacity: {
-                value: 0.5,
-                random: true,
-                anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
-            },
-            size: {
-                value: 5,
-                random: true,
-                anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
-            },
-            line_linked: {
-                enable: false,
-            },
-            move: {
-                enable: true,
-                speed: 6,
-                direction: "bottom",
-                random: false,
-                straight: false,
-                out_mode: "out",
-                bounce: false,
-                attract: { enable: false, rotateX: 600, rotateY: 1200 },
-            },
+import { tsParticles } from 'tsparticles'
+
+tsParticles.load("snow", {
+    particles: {
+        number: { value: 400, density: { enable: true, value_area: 800 } },
+        color: { value: "#fff" },
+        shape: {
+            type: ["circle", "polygon"],
+            stroke: { width: 0, color: "#000000" },
+            polygon: { nb_sides: 5, },
         },
-    });
+        opacity: {
+            value: 0.5,
+            random: true,
+            anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
+        },
+        size: {
+            value: 5,
+            random: true,
+            anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
+        },
+        line_linked: {
+            enable: false,
+        },
+        move: {
+            enable: true,
+            speed: 6,
+            direction: "bottom",
+            random: false,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+            attract: { enable: false, rotateX: 600, rotateY: 1200 },
+        },
+    },
+});
