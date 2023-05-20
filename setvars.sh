5be720611d19056d89476bae9ecc070e3bd70470
Matic Babnik <matic.babnik@outlook.com>
2023-05-20 04:49:44 +0200

    Added news
    - Added news to the home page
    - Redid the home page
    - Added a smooth transition between light and dark theme
    - Made the character limit more visible
    - The server now checks message length

--- setvars.sh
+++ setvars.sh
@@ -4,13 +4,6 @@ BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 COMMIT="$(git rev-parse HEAD)"
 DATE="$(date -Isecond)"
 
-# echo "VUE_APP_BRANCH=\"$(git rev-parse --abbrev-ref HEAD)\"" > .env
-# echo "VUE_APP_COMMIT=\"$(git rev-parse HEAD)\"">>.env
-# echo "VUE_APP_DATE=\"$(date -Isecond)\"">>.env
-
-echo "VUE_APP_BRANCH=\"$BRANCH\"" > .env
-echo "VUE_APP_COMMIT=\"$COMMIT\"" >> .env
-echo "VUE_APP_DATE=\"$DATE\"" >> .env
-echo "VITE_BRANCH=\"$BRANCH\"" >> .env
+echo "VITE_BRANCH=\"$BRANCH\"" > .env
 echo "VITE_COMMIT=\"$COMMIT\"" >> .env
 echo "VITE_DATE=\"$DATE\"" >> .env
